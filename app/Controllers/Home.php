81956253df87cb7bdec9ae50f4ec850e715afda2
rifqiadiyatma <93rifqiadiyatma@gmail.com>
2020-10-16 16:57:31 +0700

    testt

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,14 @@ class Home extends BaseController
 	{
 		return view('welcome_message');
 	}
-
+	
 	//--------------------------------------------------------------------
-
+	public function show()
+	{
+		$data['nama'] = 'Rifqi';
+		$data['npm'] = '1817051055';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer', $data);
+	}
 }
